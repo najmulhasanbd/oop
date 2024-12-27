580d36198ad2560ab2e29b8be56f8d467f230577
unknown <najmulcse247@gmail.com>
2024-12-27 21:03:51 +0600

    oop start

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Person
+{
+    public $name;
+    public $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function display()
+    {
+        return $this->name . $this->age;
+    }
+}
+
+$info = new Person('najmul', '28');
+
+echo $info->display();
+echo "<br>";
+$a = new Person('hasan', '30');
+
+echo $a->display();
