da02034ad65a88fee7399eb9e3feba2fd4ba7d48
unknown <najmulcse247@gmail.com>
2024-12-27 21:14:58 +0600

    private access done

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <?php
 
-class Person
+// class Person
+// {
+//     public $name;
+//     public $age;
+//     public function __construct($name, $age)
+//     {
+//         $this->name = $name;
+//         $this->age = $age;
+//     }
+//     public function display()
+//     {
+//         return $this->name . $this->age;
+//     }
+// }
+// $info = new Person('najmul', '28');
+// echo $info->display();
+// echo "<br>";
+// $a = new Person('hasan', '30');
+
+// echo $a->display();
+
+
+class Access
 {
     public $name;
-    public $age;
-    public function __construct($name, $age)
+    protected $age;
+    private $address;
+
+    public function hhhhh()
     {
-        $this->name = $name;
-        $this->age = $age;
-    }
-    public function display()
-    {
-        return $this->name . $this->age;
+        return 'this is private access';
     }
 }
-$info = new Person('najmul', '28');
-echo $info->display();
-echo "<br>";
-$a = new Person('hasan', '30');
 
-echo $a->display();
+$info = new Access();
+echo $info->hhhhh();
